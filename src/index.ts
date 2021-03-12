1b59a65f38d8e47c19df584b97fe036997acae6a
panospd <anastapd@gmail.com>
2021-03-12 20:47:48 +0000

    Implemented a basic eventing framework

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,15 @@ import { User } from './models/User';
 
 const user = new User({ name: 'myname', age: 20 });
 
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on('change', () => {
+  console.log('change no 1');
+});
+user.on('change', () => {
+  console.log('Change no 2');
+});
+user.on('save', () => {
+  console.log('save was triggered');
+});
 
-user.set({ name: 'new name' });
-
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.trigger('change');
+user.trigger('dsafdsa');
--- src/models/User.ts
+++ src/models/User.ts
@@ -3,7 +3,10 @@ interface UserPros {
   age?: number;
 }
 
+type Callback = () => void;
+
 export class User {
+  events: { [key: string]: Callback[] } = {};
   constructor(private data: UserPros) {}
 
   get(propName: string): string | number {
@@ -12,5 +15,21 @@ export class User {
 
   set(update: UserPros): void {
     this.data = { ...this.data, ...update };
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) return;
+
+    handlers.forEach(callback => {
+      callback();
+    });
   }
 }
